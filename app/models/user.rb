ad76505829e2befaf302546fd2179c1a4a725ca4
Ahmed Alani <s3leha@gmail.com>
2017-10-14 20:30:22 +0000

    some button styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: {minimum: 5}, allow_nil: true
+  validates :email, presence:true, uniqueness: true
+  
 end
